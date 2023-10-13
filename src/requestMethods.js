56386f707fe86aa8912f73875a1ef65626406d0a
DevKinggFemi <mayowa.odunsi01@gmail.com>
2023-10-13 06:27:18 -0700

    origin

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.baseURL = "https://e-bookstore-api.vercel.app/api/";
 axios.interceptors.request.use(function (config){
     config.headers.token = `Bearer ${localStorage.getItem("TOKEN")}`;
     return config;
 })
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://e-bookstore-api.vercel.app/api/";
 
 export const publicRequest = axios.create ({
     baseURL:BASE_URL,
